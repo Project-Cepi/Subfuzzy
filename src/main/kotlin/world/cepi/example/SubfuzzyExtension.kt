e99c0184b5679b4e83d5f13f60815144786dec35
LeoDog896 <LeoDog896@hotmail.com>
2021-12-11 09:29:50 -0500

    Fix branding

---
+++ src/main/kotlin/world/cepi/example/SubfuzzyExtension.kt
@@ -0,0 +1,15 @@
+package world.cepi.example
+
+import net.minestom.server.extensions.Extension;
+
+class SubfuzzyExtension : Extension() {
+
+    override fun initialize() {
+        logger.info("[Subfuzzy] has been enabled!")
+    }
+
+    override fun terminate() {
+        logger.info("[Subfuzzy] has been disabled!")
+    }
+
+}
