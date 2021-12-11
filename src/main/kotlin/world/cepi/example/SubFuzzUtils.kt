262c7b172e2571a9eb32a90c9ab7a4362833b031
LeoDog896 <LeoDog896@hotmail.com>
2021-12-11 14:54:53 -0500

    Fix return type

--- src/main/kotlin/world/cepi/example/SubFuzzUtils.kt
+++ src/main/kotlin/world/cepi/example/SubFuzzUtils.kt
@@ -2,6 +2,8 @@ package world.cepi.example
 
 import net.minestom.server.command.builder.arguments.Argument
 
-fun Argument<*>.attatchFuzz(fuzz: SubFuzz) = this.setCallback { sender, exception ->
-    fuzz(sender, exception.input)
+fun <T> Argument<T>.attatchFuzz(fuzz: SubFuzz): Argument<T> = apply {
+    this.setCallback { sender, exception ->
+        fuzz(sender, exception.input)
+    }
 }
